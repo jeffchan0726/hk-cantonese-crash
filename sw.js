16ebe254ac3b4f5daaed731355ac0ac9ae521c29
jeffchan0726 <jeffchan0726@gmail.com>
2026-09-09 14:17:25 +0800

    IME candidate bar 1-4 plus red character analysis (e.g. 行 = 彳/丁)

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-const CACHE = "sucheng-v7";
+const CACHE = "sucheng-v9";
 const ASSETS = ["./","index.html","css/app.css","js/app.js","js/data.js","manifest.json","icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener("fetch", (e) => {
