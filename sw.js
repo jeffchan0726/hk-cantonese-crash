4a38180ac8c5981e62a9b00077af0243e8fa1d18
jeffchan0726 <jeffchan0726@gmail.com>
2026-09-09 14:48:14 +0800

    v12: HK meme articles + cache bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sucheng-v11";
+const CACHE = "sucheng-v12";
 const ASSETS = ["./","index.html","css/app.css","js/app.js","js/data.js","manifest.json","icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
