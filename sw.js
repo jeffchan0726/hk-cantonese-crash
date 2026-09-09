741cce1223f75b14fb440044e260a64bf03bf13c
jeffchan0726 <jeffchan0726@gmail.com>
2026-09-09 14:28:13 +0800

    v10 cache bust + IME styles; restore app.js next

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sucheng-v9";
+const CACHE = "sucheng-v10";
 const ASSETS = ["./","index.html","css/app.css","js/app.js","js/data.js","manifest.json","icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
