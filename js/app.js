e7b0d0bc56f075e86c15d8616d6449bfa88d4b40
jeffchan0726 <jeffchan0726@gmail.com>
2026-09-09 14:31:35 +0800

    v10 loader for IME app (s1/s2)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-PLACEHOLDER
+(function(){
+  try {
+    var src = atob((window.__SC1||"") + (window.__SC2||""));
+    var el = document.createElement("script");
+    el.text = src;
+    document.body.appendChild(el);
+  } catch (e) {
+    var m = document.getElementById("main");
+    if (m) m.innerHTML = "<pre class='boot-err'>" + e + "</pre>";
+  }
+})();
